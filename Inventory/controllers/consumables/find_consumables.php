c38a06c467a06efb2684fa19b89e066c82ceaf5d
Paul Ian A. Dumdum <paulian.dumdum@gmail.com>
2025-09-12 13:49:06 +0800

    09122025

--- Inventory/controllers/consumables/find_consumables.php
+++ Inventory/controllers/consumables/find_consumables.php
@@ -1,16 +1,28 @@
 <?php
+    session_start();
     header('Content-Type: application/json');
     include("../../includes.php");
     $data = json_decode(file_get_contents('php://input'), true);
 
     if($data["id"]) {
         $consumables = new Consumables;
+        if($_SESSION["g_id"]){
+            $consumables = DB::find($consumables,$data["id"]);
+            $temp = [];
+            foreach ($consumables as $cons) {
+                // IF GID IS EQUAL TO GID
+            }
+            $consumables = $temp;
+        }else{
+            $consumables = DB::find($consumables,$data["id"]);
+        }
+        
         $response = [
             "status" => true,
             "type" => "info",
             "size" => null,
             "message" => "Edit consumable with ID ".$data["id"],
-            "consumable" => DB::find($consumables,$data["id"])
+            "consumable" => $consumables
         ];
     }else{
         $response = [
